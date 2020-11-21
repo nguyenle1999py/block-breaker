6806bcc2a53ac4dbe773e48fa71e686c29700f2c
Nguyen Le <nguyenle1999py@gmail.com>
2020-11-21 23:20:30 +0700

    Add game object and physical material
    add game object. rigidbody 2d,...trigger

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Im in");
+        SceneManager.LoadScene("Game Over");
+    }
+}
