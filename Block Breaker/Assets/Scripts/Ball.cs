a88abefdabcb9b1c51fb2b0a25db95aff36fd33c
Nguyen Le <nguyenle1999py@gmail.com>
2020-11-22 13:21:34 +0700

    Animation game
    Paddle move, start lauch ball

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 12f;
+    //State
+    Vector2 ballToPaddle;
+    bool hasStarted;
+    // Start is called before the first frame update'
+
+    void Start()
+    {
+        ballToPaddle = transform.position - paddle.transform.position;
+        hasStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBall();
+        }
+    }
+
+    void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + ballToPaddle;
+    }
+
+    void LaunchBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnit = 16f;
+    [SerializeField] float minWidth = 0f;
+    [SerializeField] float maxWidth = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get mouse position by width unit, default mouse position and screen width is pixel 
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidthInUnit;
+        //  Set transform x always in minWidth and maxWidth
+        float transX = Mathf.Clamp(mousePos, minWidth, maxWidth);
+        Vector2 paddlePos = new Vector2(transX, transform.position.y);
+        // Set transform (game object position) = mouse position
+        transform.position = paddlePos;
+    }
+}
